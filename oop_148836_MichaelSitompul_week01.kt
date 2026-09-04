638b0184a2539daf9067cd204018278d9259fe75
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-05 00:25:09 +0700

    week01: (tugas 2) implement discount logic

--- oop_148836_MichaelSitompul_week01.kt
+++ oop_148836_MichaelSitompul_week01.kt
@@ -1,6 +1,12 @@
-
+package oop_001_johnthor.week01
 
 fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 700000
+
+    val discountPercent = calculateDiscount(price)
+    val discountAmount = price * discountPercent / 100
+    val finalPrice = price - discountAmount
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
