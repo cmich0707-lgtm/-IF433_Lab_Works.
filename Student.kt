bde6ce1b0b1e22f4c5714094dc6d97ae08b873ec
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-05 11:00:24 +0700

    week02: challenge refactor primary constructor using default args

--- Student.kt
+++ Student.kt
@@ -1,16 +1,7 @@
 
-class Student (val name: String, val nim: String, val major) {
-    init {
-        if (nim.length !=5) {
-            println("Warning objek tercipta dengan NIM ($nim) yang tidak  valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-
-        } else {
-            println("LOG: objek student $name berhasil dialokasikan di Memory.")
-        }
-    }
-}
-construction (name: string, nim: string) : this(name, nim, major = "Non-Matriculated") {
-    println("LOG: Menggunakan construction jalur umum (Tanpa Jurusan).")
-}
-
+class student (
+    val name: String,
+    val nim: String,
+    val gpa: Double = 0.0,
+    val major: String
+) {
