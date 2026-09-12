b914700c270aa56b90714502946b0c5cd2db9838
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-12 11:34:35 +0700

    week03: (task 2) implement Player testing in main

--- Main.kt
+++ Main.kt
@@ -1,25 +1,23 @@
-class Weapon(
-    val name: String,
-    initialDamage: Int
-) {
-    var damage: Int = initialDamage
-        set(value) {
-            when {
-                value < 0 -> println("Peringatan: Damage tidak boleh negatif!")
-                value > 1000 -> field = 1000
-                else -> field = value
-            }
-        }
+fun main() {
+    // --- Testing Tugas 1: Weapon ---
+    val sword = Weapon("Excalibur", 100)
 
-    val tier: String
-        get() = when {
-            damage > 800 -> "Legendary"
-            damage > 500 -> "Epic"
-            else -> "Common"
-        }
+    // Coba set ke -50 (harus gagal/muncul peringatan)
+    sword.damage = -50
 
-    init {
-        // Validasi saat inisialisasi awal
-        this.damage = initialDamage
-    }
+    // Coba set ke 9999 (harus dipaksa jadi 1000)
+    sword.damage = 9999
+
+    // Print Tier-nya (karena damage 1000, harus "Legendary")
+    println("Tier Senjata: ${sword.tier}")
+
+    println("-----------------------------------")
+
+    // --- Testing Tugas 2: Player ---
+    val player = Player("Hero123")
+
+    // player.xp = 100 // Jika di-uncomment, ini akan ERROR (karena private)
+
+    player.addXp(50)  // Masih level 1
+    player.addXp(60)  // Total XP 110, memicu pesann level up ke level 2
 }
