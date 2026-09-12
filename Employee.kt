d93d215379fb59fd34b8217243ecf218d37b03df
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-12 10:40:20 +0700

    week03: add private property

--- Employee.kt
+++ Employee.kt
@@ -12,3 +12,14 @@ class Employee(val name: String) {
 
         }
 }
+ private var performanceRating int = 3
+
+
+fun increasePerformance() {
+    performanceRating ++
+    println("Kinerja $name meiningkat! Rating: $performanceRating")
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
+}
