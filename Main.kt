e98035310985149e44189a4a4af35c0267f09bda
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-19 10:41:45 +0700

    week04: test Vehicle and Car in main

--- Main.kt
+++ Main.kt
@@ -1,23 +1,14 @@
-fun main() {
-    // --- Testing Tugas 1: Weapon ---
-    val sword = Weapon("Excalibur", 100)
+package week4
 
-    // Coba set ke -50 (harus gagal/muncul peringatan)
-    sword.damage = -50
+fun main()   {
+    println("--- Testing Vehicle")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
 
-    // Coba set ke 9999 (harus dipaksa jadi 1000)
-    sword.damage = 9999
-
-    // Print Tier-nya (karena damage 1000, harus "Legendary")
-    println("Tier Senjata: ${sword.tier}")
-
-    println("-----------------------------------")
-
-    // --- Testing Tugas 2: Player ---
-    val player = Player("Hero123")
-
-    // player.xp = 100 // Jika di-uncomment, ini akan ERROR (karena private)
-
-    player.addXp(50)  // Masih level 1
-    player.addXp(60)  // Total XP 110, memicu pesann level up ke level 2
+    println("\n--- Testing Car---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
 }
