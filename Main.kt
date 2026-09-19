fbb29922636324ce10a8e46718ee0ca22f54b170
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-19 11:01:17 +0700

    week04: (task 2) test Employee hierarchy in main

--- Main.kt
+++ Main.kt
@@ -1,6 +1,10 @@
 fun main() {
-    val electricCar = ElectricCar("Tesla", 4, 85)
-    electricCar.accelerate()
-    electricCar.honk()
-    electricCar.openTrunk()
+    val manager = Manager("Budi", 10000000)
+    val developer = Developer("Andi", 8000000, "Kotlin")
+
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
 }
