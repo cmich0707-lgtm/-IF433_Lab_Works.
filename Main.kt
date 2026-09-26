ca824615819cd2dd25d720305e4feb53f633a218
cmich0707-lgtm <cmich0707@gmail.com>
2026-09-26 11:05:24 +0700

    week05: (task 2) implement payment polymorphism

--- Main.kt
+++ Main.kt
@@ -1,13 +1,15 @@
 package week5
 
 fun main() {
-    val mathHelper = MathHelper()
+    // Inisialisasi objek
+    val wallet = EWallet("User EWallet", 50000.0)
+    val card = CreditCard("User CreditCard", 100000.0)
 
-    val luasPersegi = mathHelper.hitungLuas(5)
-    val luasPersegiPanjang = mathHelper.hitungLuas(4, 6)
-    val luasLingkaran = mathHelper.hitungLuas(7.0)
+    // Masukkan ke dalam list bertipe PaymentMethod
+    val paymentList: List<PaymentMethod> = listOf(wallet, card)
 
-    println("Luas Persegi (sisi 5): $luasPersegi")
-    println("Luas Persegi Panjang (4x6): $luasPersegiPanjang")
-    println("Luas Lingkaran (r 7.0): $luasLingkaran")
+    // Perulangan untuk memproses pembayaran
+    for (payment in paymentList) {
+        payment.processPayment(75000.0)
+    }
 }
